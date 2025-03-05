4457c2c5926040fdfda85b73895da14714e9ed87
Hashan Chandika <hashanc@cinergix.com>
2025-03-05 11:54:29 +0530

    remove amplitude

--- src/consumers/node/index.ts
+++ src/consumers/node/index.ts
@@ -1,26 +1,9 @@
 import { Consumer } from "../../siddi";
-import { Amplitude } from "./clients/amplitude";
 import { Matomo } from "./clients/matomo";
 
-let amplitudeTracker : Amplitude;
 let matomoTracker: Matomo;
 
 export const Consumers: Record<string, Consumer> = {
-    amplitude: {
-        init: () => {
-            amplitudeTracker = new Amplitude({
-                AMPLITUDE_API_KEY: process.env.AMPLITUDE_API_KEY,
-                AMPLITUDE_TOKEN_ENDPOINT: process.env.AMPLITUDE_TOKEN_ENDPOINT as string || "https://api2.amplitude.com"
-            });
-        },
-        test: () => true,
-        identify: (userId: string, userProperties: object) => {
-            amplitudeTracker.identify(userId, userProperties);
-        },
-        track: (eventName: string, eventProperties: object) => {
-            amplitudeTracker.sendEvent(eventName, eventProperties)
-        }
-    },
     matomo: {
         init: () => {
             matomoTracker = new Matomo({
